6bb58083e5f1c621ef5ae3240d558628765624df
Miten Chauhan <miten@chauhan.nz>
2022-08-19 15:00:04 +1200

    A simple query

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
 const resolvers = {
   Query: {
+    feed: () => links,
     info: () => 'This is the API of a Hackernews Clone'
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
@@ -20,3 +33,9 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`Server is running on ${url}`);
 })
+
+const links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
