a1cf7374ff0c601da8dad6c873346e9e450179a6
Miten Chauhan <miten@chauhan.nz>
2022-08-19 13:59:08 +1200

    Getting started

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require('apollo-server')
+
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    info: () => 'This is the API of a Hackernews Clone'
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server is running on ${url}`);
+})
