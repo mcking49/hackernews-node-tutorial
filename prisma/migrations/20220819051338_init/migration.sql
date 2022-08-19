dedf569fd4c8ad695b664987dbcf515af1238646
Miten Chauhan <miten@chauhan.nz>
2022-08-19 17:20:30 +1200

    Adding a database

---
+++ prisma/migrations/20220819051338_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "description" TEXT NOT NULL,
+    "url" TEXT NOT NULL
+);
